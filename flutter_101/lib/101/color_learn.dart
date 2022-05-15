ad54037a2f6a2800b849214efc8337f031762226
Mucahid Canbey <mucahid.canbey@gmail.com>
2022-05-15 15:12:51 +0300

    Color learn eklendi| flutter_101

---
+++ flutter_101/lib/101/color_learn.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class ColorLearn extends StatelessWidget {
+  const ColorLearn({Key? key, required String title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color:Theme.of(context).errorColor,
+        child: Text('data'),
+      ),
+    );
+  }
+}
+
+class ColorItems {
+
+  static Color porchase = const Color(0xffEDBf61);
+  static Color sulu = const Color.fromRGBO(198, 237, 97, 1);
+}
--- flutter_101/lib/main.dart
+++ flutter_101/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_101/101/app_bar.dart';
 import 'package:flutter_101/101/button_learn.dart';
+import 'package:flutter_101/101/color_learn.dart';
 import 'package:flutter_101/101/container_sized_box.dart';
 import 'package:flutter_101/101/icon_learn.dart';
 import 'package:flutter_101/101/scaffold_learn.dart';
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       theme: ThemeData.dark().copyWith(
+        errorColor: ColorItems.porchase,
           appBarTheme: const AppBarTheme(
             centerTitle: true,
             systemOverlayStyle: SystemUiOverlayStyle.light,
@@ -28,7 +30,7 @@ class MyApp extends StatelessWidget {
             elevation: 0.22,
           )
       ),
-      home:  IconLearnView(title: 'icon Learn',),
+      home:  ColorLearn(title: 'Color Learn',),
     );
   }
 }
