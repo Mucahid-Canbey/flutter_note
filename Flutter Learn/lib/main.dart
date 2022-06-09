1b743d54092fe76540a0a3f55c0e8fab9775b4fb
Mucahid Canbey <mucahid.canbey@gmail.com>
2022-06-10 01:21:23 +0300

    List View Learn | 101

--- Flutter Learn/lib/main.dart
+++ Flutter Learn/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_101/101/column_row_learn.dart';
 import 'package:flutter_101/101/custom_widget_learn.dart';
 import 'package:flutter_101/101/indicator_learn.dart';
 import 'package:flutter_101/101/list_tile_learn.dart';
+import 'package:flutter_101/101/list_view_learn.dart';
 import 'package:flutter_101/101/page_view_learn.dart';
 import 'package:flutter_101/101/stack_learn.dart';
 import 'package:flutter_101/101/statefull_learn.dart';
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.transparent,
             elevation: 0.22,
           )),
-      home: const ColorLifeCycleView(
+      home: const ListViewLearn(
           //message: 'ses 2',
           // title: '',
           ),
